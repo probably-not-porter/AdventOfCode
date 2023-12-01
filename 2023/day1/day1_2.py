fe7364f12343cf4f72d6672b8390b77888d4b413
probably-not-porter <pelibby16@earlham.edu>
2023-12-01 15:13:04 -0500

    day1 part 2

--- 2023/day1/day1_2.py
+++ 2023/day1/day1_2.py
@@ -1,5 +1,28 @@
 def main():
-    pass
+    with open('input.txt') as f:
+        lines = f.readlines()
+    f.close()
 
+    TTL = 0
+    for line in lines:
+        line_vals = []
+        line_n = line.replace("\n", "")
+        print("Line: " + str(line_n))
+
+        for c in range(len(line_n)):
+            if line[c].isdigit():
+                line_vals.append(line[c])
+
+            printNums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+            for x in printNums:
+                if line[c:c+len(x)] == x:
+                    line_vals.append(printNums.index(x) + 1)
+
+        print("Line Value: " + (str(line_vals[0]) + str(line_vals[-1])))
+        TTL += int(str(line_vals[0]) + str(line_vals[-1]))
+
+    print("TOTAL: " + str(TTL))
 
 main()
+# Answer is 53855
