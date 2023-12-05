be0655583a95e8af51e81f44b4925bbe71e6e8f5
Porter Libby <pelibby16@earlham.edu>
2023-12-05 00:12:10 -0500

    work on day 3

--- 2023/day3/day3_1.go
+++ 2023/day3/day3_1.go
@@ -1,11 +1,42 @@
-f, err := os.Open(fi)
-if err != nil {
-    fmt.Printf("error opening file: %v\n",err)
-    os.Exit(1)
+package main
+import "fmt"
+import "os"
+import "bufio"
+
+// Return single lines from input buffer
+func Readln(r *bufio.Reader) (string, error) {
+    var (isPrefix bool = true
+         err error = nil
+         line, ln []byte
+        )
+    for isPrefix && err == nil {
+        line, isPrefix, err = r.ReadLine()
+        ln = append(ln, line...)
+    }
+    return string(ln),err
 }
-r := bufio.NewReader(f)
-s, e := Readln(r)
-for e == nil {
-    fmt.Println(s)
-    s,e = Readln(r)
+
+// puzzle algorithm
+func main() {
+    // READ FILE
+    f, err := os.Open("input.txt")
+    if err != nil {
+        fmt.Printf("error opening file: %v\n",err)
+        os.Exit(1)
+    }
+    r := bufio.NewReader(f)
+    s, e := Readln(r)
+
+    // MAIN LOOP
+    for e == nil {                  // for line in input
+        fmt.Println("line: " + s)   // print input line
+
+        for y := 0; y < len(s); y++ { // iterate over grid y-axis
+
+            for i, c := range "abc" { // iterate over grid x-axis
+                fmt.Println(i, " => ", string(c))
+            }
+        }
+        s,e = Readln(r) // read next line
+    }
 }
--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# AdventOfCode2021
-My solutions for Advent of Code.
+# <img height=20 src="https://adventofcode.com/favicon.png"> Advent Of Code <img height=20 src="https://adventofcode.com/favicon.png">
+![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white)
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white)
+![Shell Script](https://img.shields.io/badge/shell_script-%23121011.svg?style=for-the-badge&logo=gnu-bash&logoColor=white)
 
-https://adventofcode.com/
+__[--> AdventOfCode <--](https://adventofcode.com/)__
+
+My solutions for Advent of Code.
+Doin my best to practice different languages.
+
+```
+         * 
+        /|\                  
+       /*|O\
+      /*/|\*\
+     /X/O|*\X\
+    /*/X/|\X\*\
+   /O/*/X|*\O\X\             
+  /*/O/X/|\X\O\*\
+ /X/O/*/X|O\X\*\O\
+/O/X/*/O/|\X\*\O\X\
+        |X|      
+        |X|    
+```
+
+
 
